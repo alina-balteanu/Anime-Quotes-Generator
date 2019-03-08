0456140997262a0c971019cd2b6525709e032428
alina-balteanu <alina.balteanu@gmail.com>
2019-03-08 18:58:04 +0200

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { quotes } from "./components/quotes.js";
+import { colors } from "./components/colors.js";
+
+import "./App.scss";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: colors[0],
+      quote: quotes[0].quote,
+      author: quotes[0].author,
+      anime: quotes[0].anime
+    };
+  }
+  getStyle = prop => {
+    return {
+      [prop]: this.state.color
+    };
+  };
+  getRandom = n => {
+    return Math.floor(Math.random() * n);
+  };
+
+  onClick = e => {
+    let randomColors = this.getRandom(colors.length);
+    let randomQuotes = this.getRandom(quotes.length);
+
+    this.setState({
+      color: colors[randomColors],
+      quote: quotes[randomQuotes].quote,
+      author: quotes[randomQuotes].author,
+      anime: quotes[randomQuotes].anime
+    });
+  };
+  twitterPost = () => {
+    return (
+      "https://twitter.com/intent/tweet?hashtags=quotes&text=" +
+      this.state.quote +
+      " - " +
+      this.state.author +
+      "/" +
+      this.state.anime +
+      " -- From Anime Quote Generator"
+    );
+  };
+  tumblrPost = () => {
+    return (
+      "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&caption=" +
+      this.state.quote +
+      " - " +
+      this.state.author +
+      "/" +
+      this.state.anime +
+      " -- From Anime Quote Generator" +
+      "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
+    );
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="wrapper" style={this.getStyle("background")}>
+        <div id="quote-box">
+          <div className="quote-wrapper" style={this.getStyle("color")}>
+            <div id="text">
+              <i className="fa fa-quote-left" id="quoteicon" />
+              <span>{this.state.quote}</span>
+            </div>
+            <div id="author">
+              <span>{this.state.author}</span>
+              <span>
+                <small>from</small> {this.state.anime}
+              </span>
+            </div>
+          </div>
+
+          <div className="btn-wrapper">
+            <a
+              href={this.twitterPost()}
+              rel="noopener noreferrer"
+              target="_blank"
+              id="tweet-quote"
+              className="btn"
+              style={this.getStyle("background")}
+            >
+              <i className="btn fab fa-twitter" />
+            </a>
+            <a
+              href={this.tumblrPost()}
+              target="_blank"
+              rel="noopener noreferrer"
+              id="tumblr-quote"
+              className="btn"
+              style={this.getStyle("background")}
+            >
+              <i className="btn fab fa-tumblr" />
+            </a>
+            <a href="#!" id="new-quote" className="btn" onClick={this.onClick}>
+              <span className="btn-text" style={this.getStyle("background")}>
+                new quote
+              </span>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/quotes.js
@@ -0,0 +1,113 @@
+export let quotes = [
+  {
+    id: 1,
+    quote: "People die when they are killed.",
+    author: "Shirou Emiya",
+    anime: "Fate/Stay Night"
+  },
+  {
+    id: 2,
+    quote: "Do you have any idea how stupid we are? Don’t underestimate us.",
+    author: "Kondou Isao",
+    anime: " Gintama"
+  },
+  {
+    id: 3,
+    quote: "An apple a day keeps anyone away, if you throw it hard enough!”",
+    author: "Marie Mjolnir",
+    anime: "Soul Eater"
+  },
+  {
+    id: 4,
+    quote: "If you die, I’ll kill you!",
+    author: "Roronoa Zoro",
+    anime: "One Piece"
+  },
+  {
+    id: 5,
+    quote:
+      "When you’re in high school you should be doing things, about which you could never tell your parents!",
+    author: "Isshin Kurosaki",
+    anime: "Bleach"
+  },
+  {
+    id: 6,
+    quote: "If you use your head, you won’t get fat even if you eat sweets.",
+    author: "L",
+    anime: "Death Note"
+  },
+  {
+    id: 7,
+    quote: "And what's the real lesson? Don't leave things in the fridge.",
+    author: "Spike",
+    anime: "Cowboy Bebop"
+  },
+  {
+    id: 8,
+    quote: "Panties, Panties, Panties",
+    author: "Chii",
+    anime: "Chobits"
+  },
+  {
+    id: 9,
+    quote: "Shoot her to death, without killing her!",
+    author: "Spanda",
+    anime: "One Piece"
+  },
+  {
+    id: 10,
+    quote:
+      "Don't you know it's rude to grow mushrooms in other people's closets?!!",
+    author: "Fujioka Haruhi",
+    anime: "Ouran High School Host Club"
+  },
+  {
+    id: 11,
+    quote:
+      "It doesn't matter how negative something is. Just add 'Lol' at the end and it'll automatically become positive!",
+    author: "Monokuma ",
+    anime: "Danganronpa"
+  },
+  {
+    id: 12,
+    quote: "I'll now proceed to pleasure myself with this fish.",
+    author: "Suzaku Kururugi",
+    anime: "Code Geass"
+  },
+  {
+    id: 13,
+    quote: "Just because you're correct doesn't mean you're right",
+    author: "Shirou Emiya",
+    anime: "Fate/Stay Night"
+  },
+  {
+    id: 14,
+    quote: "This is no good. I can't win if I'm a cabbage",
+    author: "Bobobo-bo Bo-bobo",
+    anime: "Bobobo-bo Bo-bobo"
+  },
+  {
+    id: 15,
+    quote: "When i die, make me a penguin. I like penguins",
+    author: "Some character",
+    anime: "Kore Wa Zombie Desu Ka"
+  },
+  {
+    id: 16,
+    quote: "You have to take the path you have to take.",
+    author: "Hibiki Tokai",
+    anime: "Vandread"
+  },
+  {
+    id: 17,
+    quote: "Look around Eren, at these big ass trees.",
+    author: "Levi Ackerman",
+    anime: "Attack On Titan"
+  },
+  {
+    id: 17,
+    quote: "Why do women have butts on their chests?",
+    author: "Son Goku",
+    anime: "Dragon Ball Z"
+  }
+];
